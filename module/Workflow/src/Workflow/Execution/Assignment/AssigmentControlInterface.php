0407aff08606837a265f14ed4cf92432e26e3b6b
zakyalvan <zakyalvan@developware>
2013-09-28 15:10:24 +0700

    AssignmentRule -> AssignmentControl

--- module/Workflow/src/Workflow/Execution/Assignment/AssigmentControlInterface.php
+++ module/Workflow/src/Workflow/Execution/Assignment/AssigmentControlInterface.php
@@ -2,7 +2,8 @@
 namespace Workflow\Execution\Assignment;
 
 /**
- * Kontrak untuk assignment rule.
+ * Kontrak untuk assignment kontrok.
+ * Assigment kontrol menentukan assignment workitem kepada user/kelompok user.
  * 
  * @author zakyalvan
  */
