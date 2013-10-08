e16c862fafcaef595a9fde6ea96acf0e594f0c98
zakyalvan <zakyalvan@developware>
2013-10-09 06:21:02 +0700

    Sampe pagi

--- module/Contract/src/Contract/Controller/InvoiceController.php
+++ module/Contract/src/Contract/Controller/InvoiceController.php
@@ -6,11 +6,20 @@ use Zend\Mvc\Controller\AbstractActionController;
 /**
  * Kelas kontroler ini mewakili aktifitas-aktifitas dalam proses flow
  * C.4 - Penagihan dan Pembayaran Kontrak.
+ * Sebagai catatan, proses ini dimulai atau ditrigger dari aplikasi eksternal.
  *
  * @author zakyalvan
  */
 class InvoiceController extends AbstractActionController {
 	public function indexAction() {
+		
+	}
+	
+	/**
+	 * Verifikasi dan perhitungan denda.
+	 * Aktifitas C.4.2
+	 */
+	public function verify() {
 		
 	}
 }
